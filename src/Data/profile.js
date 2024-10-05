be5fd1516c4e9306718a7ebb6d0388616f619c55
Spany <110673952+SpanyDev@users.noreply.github.com>
2024-10-06 02:25:37 +0300

    Update profile.js

--- src/Data/profile.js
+++ src/Data/profile.js
@@ -2,7 +2,7 @@ const userData =  {
   name: "Spany",
   github: "spanydev",
   youtube: "spanyries",
-  profileLogo: "https://media.discordapp.net/attachments/1285694561871728726/1292264587147612197/Spany.png?ex=67031aae&is=6701c92e&hm=f3c7a048c1a2f238952b4f8440a91461790894ae45a0a4281642e9622a2e956d&=&format=webp&quality=lossless&width=473&height=473",
+  ProfileLogo: "https://media.discordapp.net/attachments/1285694561871728726/1292264587147612197/Spany.png?ex=67031aae&is=6701c92e&hm=f3c7a048c1a2f238952b4f8440a91461790894ae45a0a4281642e9622a2e956d&=&format=webp&quality=lossless&width=473&height=473",
   description:
     "Benim adım Spany, discord bot geliştiricisiyim ve aynı zamanda web geliştiricisiyim. Bu web sitesi, benimle ilgili bilgileri ve projelerimi göstermek için oluşturulmuştur.",
 };
